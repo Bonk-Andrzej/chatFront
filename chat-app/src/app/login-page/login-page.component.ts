74a8ca452c7b25aa4d149f863d747df7971ca129
PawelJastrzebski <paweljastrzebski96@gmail.com>
2018-10-05 23:39:06 +0200

    fix some bugs and add routerLing in logi-page

--- chat-app/src/app/login-page/login-page.component.ts
+++ chat-app/src/app/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
     selector: 'app-login-page',
@@ -6,8 +7,32 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
+
+    constructor(private http: HttpClient) {
+    }
+
     ngOnInit() {
     }
 
+    public addUser() {
+        const login: HTMLInputElement = document.querySelector('#login');
+        const pass: HTMLInputElement = document.querySelector('#password');
+
+        console.log(login.value);
+        console.log(pass.value);
+        const o = {nick: login.value, password: pass.value};
+
+        console.log(JSON.stringify(o));
+
+        const headers = new HttpHeaders();
+        headers.set('Content-Type', 'application/json');
+        headers.set('Access-Control-Allow-Origin', '*');
+        headers.set('Access-Control-Allow-Origin', 'true');
+        const objectObservable = this.http.post('http://51.38.133.76:90/users', {nick: login.value, password: pass.value, headers});
+
+        objectObservable.subscribe((e) => {
+        }, (e) => {
+        });
+    }
 }
 
