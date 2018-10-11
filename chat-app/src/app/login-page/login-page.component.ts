3002f5fa74cca67adaf3db92c3364c48ecc8b31f
Igor <sowinski.igor@gmail.com>
2018-10-11 11:58:26 +0200

    #1 new style

--- chat-app/src/app/login-page/login-page.component.ts
+++ chat-app/src/app/login-page/login-page.component.ts
@@ -4,7 +4,7 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 @Component({
     selector: 'app-login-page',
     templateUrl: './login-page.component.html',
-    styleUrls: ['./login-page.component.css']
+    styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
 
